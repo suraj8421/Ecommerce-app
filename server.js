240e2a3a2a5f8bbb818ef821230c1bb62b581d0c
suraj8421 <nikamsuraj7889@gmail.com>
2023-12-06 18:50:50 +0530

    final code
    MERN stack ecommerce app

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import authRoutes from "./routes/authRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoute.js";
+import path from "path";
+
+import connectDB from "./config/db.js";
+import cors from "cors";
+
+//config env
+dotenv.config();
+//databased config
+connectDB();
+//rest object
+const app = express();
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+//rest api
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`.bgCyan.white);
+});
